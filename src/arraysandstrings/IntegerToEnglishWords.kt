2479e3c0127ba85a05346acaa8ae5fca61e9e935
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2021-12-19 23:57:30 +0100

    Strings - Integer To English Words

---
+++ src/arraysandstrings/IntegerToEnglishWords.kt
@@ -0,0 +1,12 @@
+package arraysandstrings
+
+/**
+ * Convert a non-negative integer num to its English words representation.
+ */
+
+class IntegerToEnglishWords {
+    fun numberToWords(num: Int): String {
+        var result = ""
+        return result
+    }
+}
