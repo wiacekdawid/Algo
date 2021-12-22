751cf0396e92aa6d1a5560c4876a56171909f545
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2021-12-22 22:53:16 +0100

    DP - Maximum Score From Performing Multiplication Operations - first attempt solution

--- src/dp/MaximumScoreFromPerformingMultiplicationOperations.kt
+++ src/dp/MaximumScoreFromPerformingMultiplicationOperations.kt
@@ -9,7 +9,26 @@ package dp
  * Return the maximum score after performing m operations.
  */
 class MaximumScoreFromPerformingMultiplicationOperations {
-    fun maximumScore(nums: IntArray, multipliers: IntArray): Int {
+    private var memo: Array<IntArray>? = null
 
+    private fun dp(i: Int, left: Int, nums: IntArray, multipliers: IntArray): Int {
+        if (i == nums.size) {
+            return 0 // Base case
+        }
+        val mult: Int = multipliers[i]
+        val right = nums.size - 1 - (i - left)
+        if (memo?.get(i)?.get(left) ?: 0 == 0) {
+            // Recurrence relation
+            memo?.get(i)?.set(left,
+                (mult * nums[left] + dp(i + 1, left + 1, nums, multipliers))
+                    .coerceAtLeast(mult * nums[right] + dp(i + 1, left, nums, multipliers))
+            )
+        }
+        return memo?.get(i)?.get(left) ?: 0
+    }
+
+    fun maximumScore(nums: IntArray, multipliers: IntArray): Int {
+        memo = Array(nums.size) { IntArray(multipliers.size) }
+        return dp(0, 0, nums, multipliers)
     }
 }
