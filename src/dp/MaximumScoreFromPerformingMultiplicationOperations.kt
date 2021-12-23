0eb381b43932a78179e0a20f86c2327d46a0bbc2
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2021-12-23 18:42:18 +0100

    DP - Maximum Score From Performing Multiplication Operations - two working solutions

--- src/dp/MaximumScoreFromPerformingMultiplicationOperations.kt
+++ src/dp/MaximumScoreFromPerformingMultiplicationOperations.kt
@@ -9,26 +9,46 @@ package dp
  * Return the maximum score after performing m operations.
  */
 class MaximumScoreFromPerformingMultiplicationOperations {
+    /**
+     * top - down - space/time O(pow 2 multipliers.size)
+     */
     private var memo: Array<IntArray>? = null
 
     private fun dp(i: Int, left: Int, nums: IntArray, multipliers: IntArray): Int {
-        if (i == nums.size) {
+        if (i == multipliers.size) {
             return 0 // Base case
         }
-        val mult: Int = multipliers[i]
+        val currentMultiplayer = multipliers[i]
         val right = nums.size - 1 - (i - left)
         if (memo?.get(i)?.get(left) ?: 0 == 0) {
             // Recurrence relation
             memo?.get(i)?.set(left,
-                (mult * nums[left] + dp(i + 1, left + 1, nums, multipliers))
-                    .coerceAtLeast(mult * nums[right] + dp(i + 1, left, nums, multipliers))
+                (currentMultiplayer * nums[left] + dp(i + 1, left + 1, nums, multipliers))
+                    .coerceAtLeast(currentMultiplayer * nums[right] + dp(i + 1, left, nums, multipliers))
             )
         }
         return memo?.get(i)?.get(left) ?: 0
     }
 
     fun maximumScore(nums: IntArray, multipliers: IntArray): Int {
-        memo = Array(nums.size) { IntArray(multipliers.size) }
+        memo = Array(multipliers.size) { IntArray(multipliers.size) }
         return dp(0, 0, nums, multipliers)
+    }
+
+    /**
+     * bottom - up - space/time O(pow 2 multipliers.size)
+     */
+    fun maximumScore2(nums: IntArray, multipliers: IntArray): Int {
+        val dp = Array(multipliers.size+1) { IntArray(multipliers.size+1) }
+        for (i in multipliers.size-1 downTo 0) {
+            for (left in i downTo 0) {
+                val currentMultiplayer = multipliers[i]
+                val right = nums.size - 1 - (i-left)
+                val option1 = currentMultiplayer * nums[left] + dp[i+1][left+1]
+                val option2 = currentMultiplayer * nums[right] + dp[i+1][left]
+                dp[i][left] = option1.coerceAtLeast(option2)
+            }
+        }
+        return dp[0][0]
     }
 }
