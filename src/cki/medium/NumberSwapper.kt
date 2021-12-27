3835d1bfc2524a8a1eba9ad19d57591e90078ffb
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2021-12-28 00:18:45 +0100

    Medium - NumberSwapper - solution

--- src/cki/medium/NumberSwapper.kt
+++ src/cki/medium/NumberSwapper.kt
@@ -1,7 +1,20 @@
 package cki.medium
 
-class NumberSwapper {
-    fun swapNumbers(a: Integer, b: Integer) {
+/**
+ * swap numbers without temp variables
+ */
+fun main() {
+    var a = 10
+    var b = 5
+    //using addition
+    a += b
+    b = a - b
+    a -= b
+    println("a = $a  / b = $b")
 
-    }
+    // using bit operation
+    a = a.xor(b)
+    b = a.xor(b)
+    a = a.xor(b)
+    println("a = $a  / b = $b")
 }
