a9701e85cb2a14ad2511e1a0160e8174640bddec
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2021-12-29 23:07:30 +0100

    Medium - TrailingZeroes

---
+++ src/cki/medium/TrailingZeroes.kt
@@ -0,0 +1,12 @@
+package cki.medium
+
+/**
+ * Given an integer n, return the number of trailing zeroes in n!.
+ * Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+ */
+
+class TrailingZeroes {
+    fun trailingZeroes(n: Int): Int {
+
+    }
+}
