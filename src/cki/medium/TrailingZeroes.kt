20a5b5c54843e6dbca11ac25fbf24e55c64ce158
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2021-12-31 13:34:53 +0100

    Medium - TrailingZeroes - solution

--- src/cki/medium/TrailingZeroes.kt
+++ src/cki/medium/TrailingZeroes.kt
@@ -3,10 +3,17 @@ package cki.medium
 /**
  * Given an integer n, return the number of trailing zeroes in n!.
  * Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+ * time O(logN) / space O(1)
  */
 
 class TrailingZeroes {
     fun trailingZeroes(n: Int): Int {
-
+        var zeroCount = 0;
+        var currentMultiple = 5;
+        while (n >= currentMultiple) {
+            zeroCount += (n / currentMultiple)
+            currentMultiple *= 5
+        }
+        return zeroCount;
     }
 }
