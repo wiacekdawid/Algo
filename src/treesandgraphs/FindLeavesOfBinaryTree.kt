0bc22382cd41c4a68a96cf81532a568a9e489a88
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-01-07 20:01:05 +0000

    Binary Tree - FindLeavesOfBinaryTree - DFS

---
+++ src/treesandgraphs/FindLeavesOfBinaryTree.kt
@@ -0,0 +1,52 @@
+package treesandgraphs
+
+/**
+ * Given the root of a binary tree, collect a tree's nodes as if you were doing this:
+ * Collect all the leaf nodes
+ * Remove all the leaf nodes.
+ * Repeat until the tree is empty.
+ */
+class FindLeavesOfBinaryTree {
+    // solution #1 DFS space O(N) time O(2powN)
+    fun findLeaves(root: TreeNode?): List<List<Int>> {
+        val result = mutableListOf<MutableList<Int>>()
+        if (root == null) return emptyList()
+        var shouldContinue = true
+        while (shouldContinue) {
+            val subList = mutableListOf<Int>()
+            if (getLeavesAndCheckIfChildrenAreLeaves(root, subList)) {
+                shouldContinue = false
+            }
+            result.add(subList)
+        }
+        return result
+    }
+
+    private fun getLeavesAndCheckIfChildrenAreLeaves(root: TreeNode, currentList: MutableList<Int>): Boolean {
+        return if (root.left == null && root.right == null) {
+            currentList.add(root.`val`)
+            true
+        } else {
+            var leftChildIsLeave = false
+            var rightChildIsLeave = false
+            root.left?.let {
+                leftChildIsLeave = getLeavesAndCheckIfChildrenAreLeaves(it, currentList)
+            }
+            root.right?.let {
+                rightChildIsLeave = getLeavesAndCheckIfChildrenAreLeaves(it, currentList)
+            }
+            if (leftChildIsLeave) {
+                root.left = null
+            }
+            if (rightChildIsLeave) {
+                root.right = null
+            }
+            false
+        }
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+}
