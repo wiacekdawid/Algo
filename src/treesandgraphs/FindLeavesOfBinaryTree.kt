643aac85ae345e9c4729683beabd6589fad1cf06
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-01-08 12:45:23 +0000

    Binary Tree - FindLeavesOfBinaryTree - DFS using height of leaves - better time complexity

--- src/treesandgraphs/FindLeavesOfBinaryTree.kt
+++ src/treesandgraphs/FindLeavesOfBinaryTree.kt
@@ -7,7 +7,33 @@ package treesandgraphs
  * Repeat until the tree is empty.
  */
 class FindLeavesOfBinaryTree {
-    // solution #1 DFS space O(N) time O(2powN)
+    // solution #1 DFS using height of leaves space O(N) time O(N)
+    fun findLeaves2(root: TreeNode?): List<List<Int>> {
+        val solution = mutableListOf<MutableList<Int>>()
+        getHeight(root, solution)
+        return solution
+    }
+
+    private fun getHeight(root: TreeNode?, solution: MutableList<MutableList<Int>>): Int {
+        return if (root == null) {
+            -1
+        } else {
+            val leftHeight = root.left?.let { getHeight(it, solution) } ?: -1
+            val rightHeight = root.right?.let { getHeight(it, solution) } ?: -1
+
+            val currentHeight = leftHeight.coerceAtLeast(rightHeight) + 1
+
+            if (solution.size == currentHeight) {
+                solution.add(mutableListOf())
+            }
+
+            solution[currentHeight].add(root.`val`)
+
+            currentHeight
+        }
+    }
+
+    // solution #2 DFS space O(N) time O(2powN)
     fun findLeaves(root: TreeNode?): List<List<Int>> {
         val result = mutableListOf<MutableList<Int>>()
         if (root == null) return emptyList()
