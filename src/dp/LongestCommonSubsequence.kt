3aa1829ba887d47abdf1b6bfc09619e08c11aa60
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-01-10 21:41:15 +0000

    DP - LongestCommonSubsequence - time/space complexity

--- src/dp/LongestCommonSubsequence.kt
+++ src/dp/LongestCommonSubsequence.kt
@@ -5,6 +5,7 @@ package dp
  * A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
  * For example, "ace" is a subsequence of "abcde".
  * A common subsequence of two strings is a subsequence that is common to both strings.
+ * time O(M*N) / space O(min(M,N)) where M is length of first word and N is the lenght of second word
  */
 
 class LongestCommonSubsequence {
