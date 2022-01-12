e78be2f7863afb44d19e54a165ae4b25156dac25
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-01-12 22:06:18 +0000

    Find Winner on a TicTacToe Game

---
+++ src/google/easy/FindWinneronaTicTacToeGame.kt
@@ -0,0 +1,21 @@
+package google.easy
+
+/**
+ * Given a 2D integer array moves where moves[i] = [rowi, coli] indicates that the ith move will be played on grid[rowi][coli].
+ * return the winner of the game if it exists (A or B). In case the game ends in a draw return "Draw".
+ * If there are still movements to play return "Pending".
+ * You can assume that moves is valid (i.e., it follows the rules of Tic-Tac-Toe), the grid is initially empty, and A will play first.
+ */
+class FindWinneronaTicTacToeGame {
+    fun tictactoe(moves: Array<IntArray>): String {
+//        val board = Array(moves.size) { IntArray(moves.size) { 0 } }
+//
+//        var firstMove = true
+//        moves.forEach {
+//            board[it[0]][it[1]] = if (firstMove) 1 else 2
+//            firstMove = !firstMove
+//        }
+
+
+    }
+}
