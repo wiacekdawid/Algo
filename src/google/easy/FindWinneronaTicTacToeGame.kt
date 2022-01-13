4b107029b832f2f5db7bd526670fd74309b111ca
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-01-13 21:11:02 +0000

    Find Winner on a TicTacToe Game - solution

--- src/google/easy/FindWinneronaTicTacToeGame.kt
+++ src/google/easy/FindWinneronaTicTacToeGame.kt
@@ -5,17 +5,39 @@ package google.easy
  * return the winner of the game if it exists (A or B). In case the game ends in a draw return "Draw".
  * If there are still movements to play return "Pending".
  * You can assume that moves is valid (i.e., it follows the rules of Tic-Tac-Toe), the grid is initially empty, and A will play first.
+ * time O(m) where m is num of moves / space O(n) where n is length of board
  */
+
+fun main() {
+    val test = FindWinneronaTicTacToeGame().tictactoe(arrayOf(intArrayOf(2,2), intArrayOf(0,2), intArrayOf(1,0), intArrayOf(0,1), intArrayOf(2,0),
+        intArrayOf(0,0)
+    ))
+}
+
 class FindWinneronaTicTacToeGame {
     fun tictactoe(moves: Array<IntArray>): String {
-//        val board = Array(moves.size) { IntArray(moves.size) { 0 } }
-//
-//        var firstMove = true
-//        moves.forEach {
-//            board[it[0]][it[1]] = if (firstMove) 1 else 2
-//            firstMove = !firstMove
-//        }
-
-
+        val columns = IntArray(3)
+        val rows = IntArray(3)
+        var diagonal = 0
+        var antiDiagonal = 0
+        var player = 1
+        moves.forEach { move ->
+            rows[move[0]] += player
+            columns[move[1]] += player
+            if (move[0] == move[1]) {
+                diagonal += player
+            }
+            if (move[0] + move[1] == 2) {
+                antiDiagonal += player
+            }
+            if (columns.any { Math.abs(it) == 3 } ||
+                rows.any { Math.abs(it) == 3 } ||
+                Math.abs(diagonal) == 3 || Math.abs(antiDiagonal) == 3) {
+                return if (player == 1) "A" else "B"
+            }
+            player *= -1
+        }
+        return if (moves.size >= 9) "Draw"
+        else "Pending"
     }
 }
