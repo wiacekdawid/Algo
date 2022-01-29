767ca281b89bd83881816f19b716afe0d58271ad
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-01-29 10:33:47 +0000

    Logger Rate Limiter - solution

--- src/google/easy/LoggerRateLimiter.kt
+++ src/google/easy/LoggerRateLimiter.kt
@@ -10,10 +10,23 @@ package google.easy
  * Logger() Initializes the logger object.
  * bool shouldPrintMessage(int timestamp, string message) Returns true if the message should
  * be printed in the given timestamp, otherwise returns false.
+ * space O(1)/ time O(N)
  */
 
 class LoggerRateLimiter {
+    private val map = mutableMapOf<String, Int>()
     fun shouldPrintMessage(timestamp: Int, message: String): Boolean {
-        return true
+        return if (map.containsKey(message)) {
+            val currentTime = map[message] ?: -10
+            if (currentTime + 10 > timestamp) {
+                false
+            } else {
+                map[message] = timestamp
+                true
+            }
+        } else {
+            map[message] = timestamp
+            true
+        }
     }
 }
