4c9851fcfae389d8058d4125ca4075fd33b312bc
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-10-22 20:07:35 +0100

    HouseRobber - top down / bottom up solutions

--- src/dp/HouseRobber.kt
+++ src/dp/HouseRobber.kt
@@ -9,22 +9,30 @@ package dp
  *
  */
 
+fun main() {
+    val result = HouseRobber().rob2(intArrayOf(7,8,9,10,11,12))
+}
+
 class HouseRobber {
+
     /**
      * top down with memoizaiton time/space complexity O(n)
      */
     private val map = mutableMapOf<Int, Int>()
 
     fun rob(nums: IntArray): Int {
-        return dp(nums.size-1, nums)
+        return dp(0, nums)
     }
 
     private fun dp(i: Int, nums: IntArray): Int {
-        if (i == 0) return nums[i]
-        if (i == 1) return nums[1].coerceAtLeast(nums[0])
-        if (!map.containsKey(i)) {
-            map[i] = dp(i-1, nums).coerceAtLeast(dp(i-2, nums) + nums[i])
+        if (i >= nums.size) {
+            return 0
         }
+
+        if (!map.containsKey(i)) {
+            map[i] =  dp(i + 1, nums).coerceAtLeast(nums[i] + dp(i + 2, nums))
+        }
+
         return map[i] ?: 0
     }
 
@@ -32,17 +40,21 @@ class HouseRobber {
      * bottom up time/space complexity O(n)
      */
     fun rob2(nums: IntArray): Int {
-        if (nums.size == 1) return nums[0]
+        val numsSize = nums.size
 
-        val dp = IntArray(nums.size)
-
-        dp[0] = nums[0]
-        dp[1] = nums[1].coerceAtLeast(nums[0])
-
-        for (i in 2 until nums.size) {
-            dp[i] = dp[i-1].coerceAtLeast(dp[i-2] + nums[i])
+        if (numsSize == 0) {
+            return 0
         }
 
-        return dp[nums.size-1]
+        val maxRobbedAmount = IntArray(size = numsSize+1)
+
+        maxRobbedAmount[numsSize] = 0
+        maxRobbedAmount[numsSize-1] = nums[numsSize-1]
+
+        for (i in numsSize-2 downTo 0) {
+            maxRobbedAmount[i] = maxRobbedAmount[i+1].coerceAtLeast(maxRobbedAmount[i+2] + nums[i])
+        }
+
+        return maxRobbedAmount[0]
     }
 }
