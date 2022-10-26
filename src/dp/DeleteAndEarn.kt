d4817ea3ed8387481b62bc927568ceccfaa29fd7
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-10-26 20:01:47 +0100

    DeleteAndEarn - top down / bottom up fully working solutions

--- src/dp/DeleteAndEarn.kt
+++ src/dp/DeleteAndEarn.kt
@@ -1,5 +1,7 @@
 package dp
 
+import kotlin.test.currentStackTrace
+
 /**
  * You are given an integer array nums. You want to maximize the number of points you get by performing the following operation any number of times:
  * Pick any nums[i] and delete it to earn nums[i] points. Afterwards, you must delete every element equal to nums[i] - 1 and every element equal to nums[i] + 1.
@@ -12,28 +14,60 @@ fun main() {
 
 class DeleteAndEarn {
 
+    // top down time / space O(N+k) N is size of nums and k max number in nums
+    private val points = mutableMapOf<Int, Int>()
+    private val cache = mutableMapOf<Int, Int>()
+
     fun deleteAndEarn(nums: IntArray): Int {
-        return dp(nums)
-    }
-
-    private fun dp(nums: IntArray) : Int {
-        if (nums.isEmpty()) return 0
-        if (nums.size == 1) return nums[0]
-
-        var currentMax = 0
-
-        for (i in 0 until nums.size-1) {
-            val newMax = nums[i] + dp(removeElement(i, nums))
-            currentMax = currentMax.coerceAtLeast(newMax)
+        var maxNumber = 0
+        nums.forEach { currentValue ->
+            points[currentValue] = points.getOrDefault(currentValue, 0) + currentValue
+            maxNumber = maxNumber.coerceAtLeast(currentValue)
         }
-        return currentMax
+
+        return maxPoints(maxNumber)
     }
 
-    private fun removeElement(i: Int, nums: IntArray): IntArray {
-        val result = nums.toMutableList()
-        val currentElement = nums[i]
-        result.removeAt(i)
-        result.removeAll { it == currentElement+1 || it == currentElement-1 }
-        return result.toIntArray()
+    private fun maxPoints(num: Int): Int {
+        if (num == 0) {
+            return 0
+        }
+
+        if (num == 1) {
+            return points.getOrDefault(1, 0)
+        }
+
+        if (!cache.containsKey(num)) {
+            val currentGain = points.getOrDefault(num, 0)
+            cache[num] = (currentGain + maxPoints(num-2)).coerceAtLeast(maxPoints(num-1))
+        }
+
+        return cache.getOrDefault(num, 0)
     }
+
+    /** bottom up time/space O(N+k)
+     * this one could be also space optimized (O(N)) to use only last two values instead of array
+     */
+    fun deleteAndEarn2(nums: IntArray): Int {
+        val points = mutableMapOf<Int, Int>()
+
+        var maxNumber = 0
+        nums.forEach { currentValue ->
+            points[currentValue] = points.getOrDefault(currentValue, 0) + currentValue
+            maxNumber = maxNumber.coerceAtLeast(currentValue)
+        }
+
+        val maxPoints = IntArray(size = maxNumber+1)
+
+        maxPoints[0] = 0
+        maxPoints[1] = points.getOrDefault(1, 0)
+
+        for (i in 2 until maxNumber+1) {
+            val currentGain = points.getOrDefault(i, 0)
+            maxPoints[i] = (maxPoints[i-1]).coerceAtLeast(maxPoints[i-2] + currentGain)
+        }
+
+        return maxPoints[maxNumber]
+    }
+
 }
