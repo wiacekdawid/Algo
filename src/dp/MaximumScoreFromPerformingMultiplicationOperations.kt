30bb4ebdeb0c9bc91c729d6c7aea4a8c71a88339
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-10-28 21:31:11 +0100

    MaximumScoreFromPerformingMultiplicationOperations - top down fully working solutions

--- src/dp/MaximumScoreFromPerformingMultiplicationOperations.kt
+++ src/dp/MaximumScoreFromPerformingMultiplicationOperations.kt
@@ -8,21 +8,40 @@ package dp
  * Remove x from the array nums.
  * Return the maximum score after performing m operations.
  */
+
+fun main() {
+    val test = MaximumScoreFromPerformingMultiplicationOperations()
+        .maximumScore(nums = intArrayOf(1,2,3), multipliers = intArrayOf(3,2,1))
+    val test1 = test
+}
+
 class MaximumScoreFromPerformingMultiplicationOperations {
+
+    private var cache: Array<IntArray>? = null
+
     fun maximumScore(nums: IntArray, multipliers: IntArray): Int {
+        cache = Array(multipliers.size) { IntArray(nums.size) { -1 } }
         return dp(0, 0, nums, multipliers)
     }
 
-    fun dp(i: Int, left: Int, nums: IntArray, multipliers: IntArray) : Int {
+    private fun dp(i: Int, left: Int, nums: IntArray, multipliers: IntArray) : Int {
         if (i == multipliers.size) {
             return 0
         }
-        val right = nums.size - 1 - (i - left)
 
-        val leftGain = multipliers[i] * nums[left] + dp(i+1, left+1, nums, multipliers)
-        val rightGain = multipliers[i] * nums[right] + dp(i+1, left, nums, multipliers)
+        cache?.let {
+            if (it[i][left] != -1) {
+                return it[i][left]
+            }
+            val right = nums.size - 1 - (i - left)
 
-        return leftGain.coerceAtLeast(rightGain)
+            val leftGain = multipliers[i] * nums[left] + dp(i + 1, left + 1, nums, multipliers)
+            val rightGain = multipliers[i] * nums[right] + dp(i + 1, left, nums, multipliers)
+
+            it[i][left] = leftGain.coerceAtLeast(rightGain)
+            return it[i][left]
+
+        } ?: return 0
     }
 //    /**
 //     * top - down - space/time O(pow 2 multipliers.size)
