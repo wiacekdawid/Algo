e82815fd55365a301b2ce7b46de833eded6d0e64
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-11-26 23:38:51 +0000

    WordBreak - top down not fully working solution

--- src/dp/patterns/WordBreak.kt
+++ src/dp/patterns/WordBreak.kt
@@ -7,11 +7,32 @@ package dp.patterns
  */
 class WordBreak {
     // top down
+
+    lateinit var cache: IntArray
+
     fun wordBreak(s: String, wordDict: List<String>): Boolean {
-        return false
+        cache = IntArray(s.length)
+        return dp(s.length, s, wordDict)
     }
 
-    private fun dp(i: Integer): Boolean {
-        return false
+    private fun dp(i: Int, s: String, wordDict: List<String>): Boolean {
+        if (i < 0) return true
+
+        if (cache[i] == -1) {
+            for (word in wordDict) {
+                if (i >= word.length - 1 && dp(i - word.length, s, wordDict)) {
+                    if (s.substring(i - word.length + 1, i + 1) == word) {
+                        cache[i] = 1
+                        break
+                    }
+                }
+            }
+        }
+
+        if (cache[i] == -1) {
+            cache[i] = 0
+        }
+
+        return cache[i] == 1
     }
 }
