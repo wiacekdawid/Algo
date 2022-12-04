4f77772c42e1fb2cd2c003a68f06d534d25dd2b6
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2022-12-04 22:24:51 +0000

    LongestIncreasingSubsequence - simplified version of dynamic programming

--- src/dp/patterns/LongestIncreasingSubsequence.kt
+++ src/dp/patterns/LongestIncreasingSubsequence.kt
@@ -5,10 +5,32 @@ package dp.patterns
  */
 
 fun main() {
-    val test = LongestIncreasingSubsequence().lengthOfLIS(intArrayOf(0,1,0,3,2,3))
+    val test = LongestIncreasingSubsequence().lengthOfLIS2(intArrayOf(0,1,0,3,2,3))
 }
 
 class LongestIncreasingSubsequence {
+
+    // simplified time O(n pow 2) , time O(n)
+    fun lengthOfLIS2(nums: IntArray): Int {
+        val cache = IntArray(nums.size) { 1 }
+        for (i in 1 until nums.size)
+            for (j in 0 until i) {
+                if (nums[i] > nums[j]) {
+                    cache[i] = cache[i].coerceAtLeast(cache[j] + 1)
+                }
+            }
+
+
+        var maxValue = 0
+
+        cache.forEach {
+            maxValue = maxValue.coerceAtLeast(it)
+        }
+
+        return maxValue
+    }
+
+    // recursion
     private lateinit var maxForPosition: IntArray
     fun lengthOfLIS(nums: IntArray): Int {
         maxForPosition = IntArray(nums.size) { -1 }
