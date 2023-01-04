7990939bbd3230b02825df2b6d9114bd096fbac5
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-01-04 20:42:05 +0000

    BestTimeToBuyAndSellStockIV

---
+++ src/dp/patterns/BestTimeToBuyAndSellStockIV.kt
@@ -0,0 +1,13 @@
+package dp.patterns
+
+/**
+ * You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
+ * Find the maximum profit you can achieve. You may complete at most k transactions.
+ * Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+ */
+
+class BestTimeToBuyAndSellStockIV {
+    fun maxProfit(k: Int, prices: IntArray): Int {
+
+    }
+}
