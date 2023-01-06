9482969ef509c853d96d84bd0b77ff3b8494fbb0
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-01-06 22:07:52 +0000

    BestTimeToBuyAndSellStockIV - top down solution

--- src/dp/patterns/BestTimeToBuyAndSellStockIV.kt
+++ src/dp/patterns/BestTimeToBuyAndSellStockIV.kt
@@ -7,7 +7,30 @@ package dp.patterns
  */
 
 class BestTimeToBuyAndSellStockIV {
-    fun maxProfit(k: Int, prices: IntArray): Int {
 
+    // top down time and space - O(n*k)
+    private lateinit var memo: Array<Array<IntArray>>
+
+    fun maxProfit(k: Int, prices: IntArray): Int {
+        memo = Array(prices.size) { Array(k+1) { IntArray(2) { -1 } } }
+        return dp(0, k, 0, prices)
+    }
+
+    private fun dp(currentPrice: Int, transactionsRemaining: Int, holdingStock: Int, prices: IntArray): Int {
+
+        if (currentPrice >= prices.size || transactionsRemaining == 0) return 0
+
+        if (memo[currentPrice][transactionsRemaining][holdingStock] == -1) {
+            val doNothing = dp(currentPrice+1, transactionsRemaining, holdingStock, prices)
+
+            val doSomething = if (holdingStock == 0) {
+                dp(currentPrice + 1, transactionsRemaining, 1, prices) - prices[currentPrice]
+            } else {
+                dp(currentPrice + 1, transactionsRemaining-1, 0, prices) + prices[currentPrice]
+            }
+            memo[currentPrice][transactionsRemaining][holdingStock] = doNothing.coerceAtLeast(doSomething)
+        }
+
+        return memo[currentPrice][transactionsRemaining][holdingStock]
     }
 }
