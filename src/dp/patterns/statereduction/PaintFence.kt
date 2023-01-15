b4d04d9fefab8646f82bd60bf90080d649a33bc9
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-01-15 17:23:34 +0000

    PaintFence - bottom up solution (with opt space complexity)

--- src/dp/patterns/statereduction/PaintFence.kt
+++ src/dp/patterns/statereduction/PaintFence.kt
@@ -24,14 +24,53 @@ class PaintFence {
                 memo[currentPost] = k
             }
             else if (currentPost == 2) {
-                memo[currentPost] =  k * k
+                memo[currentPost] =  k + k * (k-1)
             }
             else {
                 memo[currentPost] = (k-1) * (dp(currentPost-1, k) + dp(currentPost-2, k))
             }
 
         }
-
         return memo[currentPost]
+    }
+
+    // bottom up space / time O(n)
+    fun numWays2(n: Int, k: Int): Int {
+        val numOfCombinations = IntArray(n) { 0 }
+
+        for (currentPost in 0 until n) {
+            when (currentPost) {
+                0 -> {
+                    numOfCombinations[currentPost] = k
+                }
+                1 -> {
+                    numOfCombinations[currentPost] = k*k
+                }
+                else -> {
+                    numOfCombinations[currentPost] = (k-1) *  (numOfCombinations[currentPost-1] + numOfCombinations[currentPost-2])
+                }
+            }
+        }
+        return numOfCombinations[n-1]
+    }
+
+    // bottom up space O(1) / time O(n)
+    fun numWays3(n: Int, k: Int): Int {
+        if (n == 1)
+            return k
+        if (n == 2)
+            return k*k
+
+        var previousTheSame = k
+        var previousDifferent = k * (k-1)
+        var total = previousDifferent + previousTheSame
+        for (currentPost in 2 until n) {
+            var theSame = previousDifferent
+            var different = (k-1) * (previousTheSame + previousDifferent)
+            total = theSame + different
+            previousTheSame = theSame
+            previousDifferent = different
+        }
+        return total
     }
 }
