6de81db7ab4c17fb2b74ff75c470bf963f42480c
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-01-18 22:13:04 +0000

    CoinChange2 - bottom up solutions

--- src/dp/patterns/statereduction/CoinChange2.kt
+++ src/dp/patterns/statereduction/CoinChange2.kt
@@ -1,17 +1,25 @@
 package dp.patterns.statereduction
 
+import kotlin.math.sign
+
 /**
  * You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
  * Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
  * You may assume that you have an infinite number of each kind of coin.
  * The answer is guaranteed to fit into a signed 32-bit integer.
  */
+
+// bottom up time O(n * amount) / space O(amount)
 class CoinChange2 {
     fun change(amount: Int, coins: IntArray): Int {
+        val cache = IntArray(amount+1)
+        cache[0] = 1
 
-    }
-
-    private fun dp() {
-
+        coins.forEach { currentCoin ->
+            for (currentAmount in currentCoin until amount+1) {
+                cache[currentAmount] += cache[currentAmount - currentCoin]
+            }
+        }
+        return cache[amount]
     }
 }
