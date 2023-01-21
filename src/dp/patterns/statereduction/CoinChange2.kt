2050f6b3560d9a75d980ff2514afb5b1788bcc6d
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-01-21 23:51:19 +0000

    CoinChange2 - top down 2D solutions

--- src/dp/patterns/statereduction/CoinChange2.kt
+++ src/dp/patterns/statereduction/CoinChange2.kt
@@ -36,6 +36,36 @@ class CoinChange2 {
         return c1 + c2
     }
 
+    // top down 2D time O(n * amount) / space O(amount)
+    private lateinit var cache: Array<IntArray>
+
+    fun change2(amount: Int, coins: IntArray): Int {
+        cache = Array(amount+1) { IntArray(coins.size) { -1 } }
+        return dp(0, amount, coins)
+    }
+
+    private fun dp(currentCoin: Int, currentAmount: Int, coins: IntArray): Int {
+        if (currentAmount == 0)
+            return 1
+
+        if (currentCoin >= coins.size)
+            return 0
+
+        if (cache[currentAmount][currentCoin] == -1) {
+            val currentCoin = coins[currentCoin]
+            var c1 = 0
+
+            if ((currentAmount - currentCoin) >= 0)
+                c1 = dp(currentCoin, currentAmount - currentCoin, coins)
+
+            val c2 = dp(currentCoin+1, currentAmount, coins)
+
+            cache[currentAmount][currentCoin] = c1 + c2
+        }
+
+        return cache[currentAmount][currentCoin]
+    }
+
     // bottom up time O(n * amount) / space O(amount)
     fun change(amount: Int, coins: IntArray): Int {
         val cache = IntArray(amount+1)
@@ -47,35 +77,5 @@ class CoinChange2 {
             }
         }
         return cache[amount]
-    }
-
-    // top down time O(n * amount) / space O(amount)
-    private lateinit var cache: IntArray
-
-    fun change2(amount: Int, coins: IntArray): Int {
-        cache = IntArray(amount+1) { -1 }
-        cache[0] = 1
-
-        coins.forEach { currentCoin ->
-            for (currentAmount in currentCoin until amount+1) {
-                cache[currentAmount] += cache[currentAmount - currentCoin]
-            }
-        }
-        return cache[amount]
-    }
-
-    private fun dp(currentAmount: Int, amount: Int, coins: IntArray): Int {
-        if (currentAmount == 0)
-            return 1
-
-        if (cache[currentAmount] == -1) {
-            coins.forEach { currentCoin ->
-                for (currentAmount in currentCoin until amount+1) {
-                    cache[currentAmount] =
-                }
-            }
-        }
-
-        return cache[currentAmount]
     }
 }
