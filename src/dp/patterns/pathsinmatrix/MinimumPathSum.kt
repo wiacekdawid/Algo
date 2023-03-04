8c3da74521e7fbed4d822c2004f68a20bcf6cd34
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-03-04 11:13:19 +0000

    MinimumPathSum - bottom up

--- src/dp/patterns/pathsinmatrix/MinimumPathSum.kt
+++ src/dp/patterns/pathsinmatrix/MinimumPathSum.kt
@@ -5,20 +5,30 @@ package dp.patterns.pathsinmatrix
  * from top left to bottom right, which minimizes the sum of all numbers along its path.
  * Note: You can only move either down or right at any point in time.
  */
+
+fun main() {
+    val test = MinimumPathSum().minPathSum(arrayOf(intArrayOf(1,3,1), intArrayOf(1,5,1), intArrayOf(4,2,1)))
+}
+
 class MinimumPathSum {
+    /**
+     * bottom up space/time complexity O(m*x)
+     */
     fun minPathSum(grid: Array<IntArray>): Int {
         val dp = Array(size = grid.size) { IntArray(size = grid.first().size) }
         dp[0][0] = grid[0][0]
 
         for (row in grid.indices) {
             for (column in 0 until grid.first().size) {
-                val rowValue = if (row > 0) {
-                    dp[row-1][column]
-                } else 0
-                val columnValue = if (column > 0) {
-                    dp[row][column-1]
-                } else 0
-                dp[row][column] = grid[row][column] + rowValue.coerceAtMost(columnValue)
+                if (row != 0 || column != 0) {
+                    val rowValue = if (row > 0) {
+                        dp[row-1][column]
+                    } else Int.MAX_VALUE
+                    val columnValue = if (column > 0) {
+                        dp[row][column-1]
+                    } else Int.MAX_VALUE
+                    dp[row][column] = grid[row][column] + rowValue.coerceAtMost(columnValue)
+                }
             }
         }
         return dp[grid.size-1][grid.first().size-1]
