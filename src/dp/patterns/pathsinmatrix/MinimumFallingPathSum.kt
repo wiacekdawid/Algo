5332d6bec001a81d4407d8331cf729182a0ad3e9
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-03-07 08:10:10 +0000

    MinimumFallingPathSum - top down solution

--- src/dp/patterns/pathsinmatrix/MinimumFallingPathSum.kt
+++ src/dp/patterns/pathsinmatrix/MinimumFallingPathSum.kt
@@ -18,14 +18,13 @@ class MinimumFallingPathSum {
                 if (row == 0) {
                     dp[row][column] = matrix[row][column]
                 } else {
-
                     val columnValue1 = if (column > 0) {
                         dp[row-1][column-1]
                     } else Int.MAX_VALUE
                     val columnValue2 = if (column < matrix.first().size-1) {
-                        dp[row][column+1]
+                        dp[row-1][column+1]
                     } else Int.MAX_VALUE
-                    dp[row][column] = matrix[row][column] + dp[row][column+1].coerceAtMost(columnValue1.coerceAtMost(columnValue2))
+                    dp[row][column] = matrix[row][column] + dp[row-1][column].coerceAtMost(columnValue1.coerceAtMost(columnValue2))
                 }
             }
         }
@@ -35,5 +34,38 @@ class MinimumFallingPathSum {
             result = result.coerceAtMost(dp.last()[column])
         }
         return result
+    }
+
+    /**
+     * top down space/time complexity O(m*x)
+     */
+    private lateinit var cache: Array<IntArray>
+
+    fun minFallingPathSum2(matrix: Array<IntArray>): Int {
+        cache = Array(size = matrix.size) { IntArray(size = matrix.first().size) { -1 } }
+
+        var result = Int.MAX_VALUE
+        for (column in matrix.first().indices) {
+            result = result.coerceAtMost(dp(matrix.size-1, column, matrix))
+        }
+        return result
+    }
+
+    private fun dp(row: Int, column: Int, matrix: Array<IntArray>): Int {
+        if (cache[row][column] == -1) {
+            if (row == 0) {
+                cache[row][column] = matrix[row][column]
+            } else {
+                val columnValue1 = if (column > 0) {
+                    dp(row-1, column-1, matrix)
+                } else Int.MAX_VALUE
+                val columnValue2 = if (column < matrix.first().size-1) {
+                    dp(row-1, column+1, matrix)
+                } else Int.MAX_VALUE
+                cache[row][column] = matrix[row][column] + dp(row-1, column, matrix).coerceAtMost(columnValue1.coerceAtMost(columnValue2))
+            }
+        }
+
+        return cache[row][column]
     }
 }
