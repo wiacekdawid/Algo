9c7266a99f744cdb285f2517759c7569061e70e5
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-03-30 22:23:19 +0100

    Count Vowels Permutation - top down solution

--- src/dp/more/CountVowelsPermutation.kt
+++ src/dp/more/CountVowelsPermutation.kt
@@ -1,5 +1,7 @@
 package dp.more
 
+import kotlin.math.pow
+
 /**
  * Given an integer n, your task is to count how many strings of length n can be formed under the following rules:
  * Each character is a lower case vowel ('a', 'e', 'i', 'o', 'u')
@@ -12,8 +14,44 @@ package dp.more
  */
 
 class CountVowelsPermutation {
-    lateinit var dp: Aray<Int>
+
+    // top down space/time
+    lateinit var dp: Array<IntArray>
+    private val map = mapOf('a' to 0, 'e' to 1, 'i' to 2, 'o' to 3, 'u' to 4)
+    private val maxValue = 10.0.pow(9.0) + 7
     fun countVowelPermutation(n: Int): Int {
-        return 0
+        dp = Array(n+1) { IntArray(map.size+1) { -1 } }
+        var result = 0
+        map.forEach {
+            result += dp(n, it.key)
+        }
+        if (result > maxValue || result < 0) return maxValue.toInt()
+        return result
+    }
+
+    private fun dp(n: Int, previousVowel: Char): Int {
+        if (n == 1) {
+            return 1
+        }
+        if (dp[n][map.getOrDefault(previousVowel, 0)] == -1) {
+            dp[n][map.getOrDefault(previousVowel, 0)] = when (previousVowel) {
+                'a' -> {
+                    dp(n-1, 'e')
+                }
+                'e' -> {
+                    dp(n-1, 'a') + dp(n-1, 'i')
+                }
+                'i' -> {
+                    dp(n-1, 'a') + dp(n-1, 'e') + dp(n-1, 'o') + dp(n-1, 'u')
+                }
+                'o' -> {
+                    dp(n-1, 'i') + dp(n-1, 'u')
+                }
+                else -> {
+                    dp(n-1, 'a')
+                }
+            }
+        }
+        return dp[n][map.getOrDefault(previousVowel, 0)]
     }
 }
