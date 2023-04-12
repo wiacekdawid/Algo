2bf7c8ad6c4618e6ce7ebab34fe89134178b2f0b
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-04-12 23:50:47 +0100

    Middle Of The Linked List

---
+++ src/75plan/level1/MiddleOfTheLinkedList.kt
@@ -0,0 +1,17 @@
+package `75plan`.level1
+
+
+/**
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ * If there are two middle nodes, return the second middle node.
+ */
+
+class MiddleOfTheLinkedList {
+    fun middleNode(head: ListNode?): ListNode? {
+        return head
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+        }
+}
