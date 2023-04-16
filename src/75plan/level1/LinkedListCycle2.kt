99fc12799406fe5646d77cfef948e45543f1c1b4
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-04-16 22:38:10 +0100

    Linked List Cycle 2 - intersect solution - Floyds algorithm

--- src/75plan/level1/LinkedListCycle2.kt
+++ src/75plan/level1/LinkedListCycle2.kt
@@ -15,13 +15,36 @@ class LinkedListCycle2 {
         return null
     }
 
+    // time O(n) / space O(1) - Floyd algorithm
     fun detectCycle2(head: ListNode?): ListNode? {
+        val intersection = findIntersection(head)
 
+        if (intersection != null) {
+            var currentHead = head
+            var currIntersection = intersection
+
+            while (currentHead != currIntersection) {
+                currentHead = currentHead?.next
+                currIntersection = currIntersection?.next
+            }
+            return currentHead
+        }
+
+        return null
     }
 
     private fun findIntersection(head: ListNode?): ListNode? {
-        val slow = head
-        val fast = head
+        var slow = head
+        var fast = head
+
+        while(fast != null && fast.next != null) {
+            slow = slow?.next
+            fast = fast.next?.next
+            if (fast == slow) {
+                return fast
+            }
+        }
+        return null
     }
 
     class ListNode(var `val`: Int) {
