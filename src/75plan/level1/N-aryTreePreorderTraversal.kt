d5a19e92ffc52fa588abc02bbdb7bc4ba1115259
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-04-20 19:49:33 +0100

    N-ary Tree Preorder Traversal

---
+++ src/75plan/level1/N-aryTreePreorderTraversal.kt
@@ -0,0 +1,15 @@
+package `75plan`.level1
+
+/**
+ * Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+ * Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+ */
+class `N-aryTreePreorderTraversal` {
+    fun preorder(root: Node?): List<Int> {
+
+    }
+
+    class Node(var `val`: Int) {
+        var children: List<Node?> = listOf()
+    }
+}
