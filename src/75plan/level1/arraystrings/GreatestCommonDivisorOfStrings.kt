2bfecaf395a64cdcc13d1d538bc5efcc7b5f6a50
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-04-25 23:56:44 +0100

    Greatest Common Divisor Of Strings

---
+++ src/75plan/level1/arraystrings/GreatestCommonDivisorOfStrings.kt
@@ -0,0 +1,13 @@
+package `75plan`.
+
+/**
+ * For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+ * Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+ */
+
+
+class GreatestCommonDivisorOfStrings {
+    fun gcdOfStrings(str1: String, str2: String): String {
+
+    }
+}
