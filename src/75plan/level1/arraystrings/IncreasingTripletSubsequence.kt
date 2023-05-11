a6a86dcf55b919f0a83398d00db82e42332b96ce
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-05-11 22:07:56 +0100

    Increasing Triplet Subsequence

--- src/75plan/level1/arraystrings/IncreasingTripletSubsequence.kt
+++ src/75plan/level1/arraystrings/IncreasingTripletSubsequence.kt
@@ -4,9 +4,27 @@ package `75plan`.level1.arraystrings
  * Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k].
  * If no such indices exists, return false.
  */
+
+fun main() {
+    val test = IncreasingTripletSubsequence().increasingTriplet(intArrayOf(20, 100, 10, 12, 5, 13))
+}
 class IncreasingTripletSubsequence {
+    // time O(n)/ space O(1)
     fun increasingTriplet(nums: IntArray): Boolean {
-        nums.forEachIndexed { index, i ->  }
+        var smallest = nums.first()
+        var medium = Integer.MIN_VALUE
+        for (i in 1 until nums.size) {
+            if (nums[i] < smallest) {
+                smallest = nums[i]
+                medium = Integer.MIN_VALUE
+            } else if (medium > smallest) {
+                if (nums[i] > medium) {
+                    return true
+                }
+            } else if (nums[i] > smallest) {
+                medium = nums[i]
+            }
+        }
         return false
     }
 }
