f5781269ac46a931332647c48043c28d7f4c0923
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-05-26 21:49:28 +0100

    Move Zeroes

--- src/75plan/twopointers/MoveZeroes.kt
+++ src/75plan/twopointers/MoveZeroes.kt
@@ -6,7 +6,25 @@ package `75plan`.twopointers
  */
 
 class MoveZeroes {
+    // time O(n) space O(1)
     fun moveZeroes(nums: IntArray): Unit {
+        var currInd = 0
+        var numOfZeroes = 0
 
+        while (currInd < nums.size) {
+            if (nums[currInd] != 0) {
+                nums[currInd - numOfZeroes] = nums[currInd]
+            } else {
+                numOfZeroes++
+            }
+            currInd++
+        }
+
+        currInd = nums.size - numOfZeroes
+
+        while(currInd < nums.size) {
+            nums[currInd] = 0
+            currInd++
+        }
     }
 }
