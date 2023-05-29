58b85be6089ff4883da9f7a7c4732d00ae112cb3
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-05-29 23:54:17 +0100

    MaxNumberOfK-SumPairs

---
+++ src/75plan/twopointers/MaxNumberOfK-SumPairs.kt
@@ -0,0 +1,13 @@
+package `75plan`.twopointers
+
+/**
+ * You are given an integer array nums and an integer k.
+ * In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+ * Return the maximum number of operations you can perform on the array.
+ */
+
+class `MaxNumberOfK-SumPairs` {
+    fun maxOperations(nums: IntArray, k: Int): Int {
+
+    }
+}
