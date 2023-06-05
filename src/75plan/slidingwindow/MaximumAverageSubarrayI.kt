c127cf5c132591ff8974a7a3283b319a7f59ab2a
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-06-05 23:37:45 +0100

    Maximum Average Subarray I

---
+++ src/75plan/slidingwindow/MaximumAverageSubarrayI.kt
@@ -0,0 +1,11 @@
+package `75plan`.slidingwindow
+
+/**
+ * You are given an integer array nums consisting of n elements, and an integer k.
+ * Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+ */
+class MaximumAverageSubarrayI {
+    fun findMaxAverage(nums: IntArray, k: Int): Double {
+
+    }
+}
