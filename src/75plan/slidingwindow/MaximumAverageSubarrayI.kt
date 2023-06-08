ffcfe811f2baacfb213f0133cec83d27f0a6e827
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-06-08 22:33:33 +0100

    Maximum Average Subarray I

--- src/75plan/slidingwindow/MaximumAverageSubarrayI.kt
+++ src/75plan/slidingwindow/MaximumAverageSubarrayI.kt
@@ -4,21 +4,29 @@ package `75plan`.slidingwindow
  * You are given an integer array nums consisting of n elements, and an integer k.
  * Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
  */
+
+fun main() {
+    val test = MaximumAverageSubarrayI().findMaxAverage(intArrayOf(1,12,-5,-6,50,3), 4)
+}
+
+// time O(n) / space O(1)
 class MaximumAverageSubarrayI {
     fun findMaxAverage(nums: IntArray, k: Int): Double {
         if (nums.size < k) return 0.0
 
-        val currentArray = IntArray(size = k) { Integer.MIN_VALUE }
-        var currentMin = Integer.MAX_VALUE
+        var currentSum = 0.0
 
-        nums.forEachIndexed { index, i ->
-            if (i < k) {
-                currentArray[index] = i
-                currentMin = i.coerceAtMost(currentMin)
-            } else {
-                if (i )
-            }
-
+        for (i in 0 until k) {
+            currentSum += nums[i]
         }
+
+        var currentMax = currentSum / k
+
+        for ((indexMin, indexMax) in (k until nums.size).withIndex()) {
+            currentSum = currentSum - nums[indexMin] + nums[indexMax]
+            currentMax = currentMax.coerceAtLeast(currentSum/k)
+        }
+
+        return currentMax
     }
 }
