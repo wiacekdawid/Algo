296495b5d4959e4cf1568b9758ff2af47ebd1d7b
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-06-09 23:45:10 +0100

    Maximum Number of Vowels in a Substring of Given Length

---
+++ src/75plan/slidingwindow/MaximumNumberOfVowelsinaSubstringofGivenLength.kt
@@ -0,0 +1,12 @@
+package `75plan`.slidingwindow
+
+/**
+ * Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+ * Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+ */
+
+class MaximumNumberOfVowelsinaSubstringofGivenLength {
+    fun maxVowels(s: String, k: Int): Int {
+
+    }
+}
