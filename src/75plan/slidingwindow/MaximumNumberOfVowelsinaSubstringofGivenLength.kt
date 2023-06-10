bf98e272da2c24c598bbcdd7b31d155e03dba13d
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-06-10 19:05:45 +0100

    Maximum Number of Vowels in a Substring of Given Length

--- src/75plan/slidingwindow/MaximumNumberOfVowelsinaSubstringofGivenLength.kt
+++ src/75plan/slidingwindow/MaximumNumberOfVowelsinaSubstringofGivenLength.kt
@@ -6,7 +6,35 @@ package `75plan`.slidingwindow
  */
 
 class MaximumNumberOfVowelsinaSubstringofGivenLength {
+    // time O(n) / space O(1)
     fun maxVowels(s: String, k: Int): Int {
+        if (s.length < k) {
+            return 0
+        }
+        var maxVowels = 0
 
+        for (i in 0 until k) {
+            if (isVowel(s[i])) {
+                maxVowels++
+            }
+        }
+
+        var currentMax = maxVowels
+        var minIndex = 1
+
+        for (maxIndex in k until s.length) {
+            if (isVowel(s[minIndex-1])) {
+                currentMax--
+            }
+            if (isVowel(s[maxIndex])) {
+                currentMax++
+            }
+            minIndex++
+            maxVowels = maxVowels.coerceAtLeast(currentMax)
+        }
+        return maxVowels
     }
+
+    private fun isVowel(c: Char) =
+        c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
 }
