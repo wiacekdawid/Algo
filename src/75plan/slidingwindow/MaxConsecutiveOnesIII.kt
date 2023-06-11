4b4c03752172427bfe08863d7cea9ede27799970
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-06-11 21:32:41 +0100

    Max Consecutive Ones III

---
+++ src/75plan/slidingwindow/MaxConsecutiveOnesIII.kt
@@ -0,0 +1,21 @@
+package `75plan`.slidingwindow
+
+/**
+ * Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+ */
+
+class MaxConsecutiveOnesIII {
+    fun longestOnes(nums: IntArray, k: Int): Int {
+        if (nums.isEmpty()) return 0
+        var numOfNonOnes = 0
+        var maxLenghtOfString = 0
+
+        var leftIndex = 0
+
+        for (rightIndex in 1 until nums.size) {
+
+        }
+
+        return maxLenghtOfString
+    }
+}
