9023d401b35c6dd456c366d74cce56e90f0d53dc
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-06-15 23:19:02 +0100

    Longest Subarray of 1's After Deleting One Element

--- src/75plan/slidingwindow/LongestSubarrayOf1sAfterDeletingOneElement.kt
+++ src/75plan/slidingwindow/LongestSubarrayOf1sAfterDeletingOneElement.kt
@@ -6,9 +6,36 @@ package `75plan`.slidingwindow
  */
 
 class LongestSubarrayOf1sAfterDeletingOneElement {
+    // time O(n) / space O(1)
     fun longestSubarray(nums: IntArray): Int {
-        var maxSize = 0
-        var minIndex = 0
-        return maxSize
+        if (nums.isEmpty()) return 0
+        var numOfNonOnes = 0
+        var maxLengthOfString = 0
+        var currentLength = 0
+
+        var leftIndex = 0
+
+        for (rightIndex in nums.indices) {
+            if (nums[rightIndex] == 0) {
+                numOfNonOnes++
+                if (numOfNonOnes > 1) {
+                    while (numOfNonOnes > 1) {
+                        if (nums[leftIndex] == 0) {
+                            numOfNonOnes--
+                        } else {
+                            currentLength--
+                        }
+                        leftIndex++
+                    }
+                } else {
+                    currentLength++
+                }
+            } else {
+                currentLength++
+            }
+            maxLengthOfString = maxLengthOfString.coerceAtLeast(currentLength-1)
+        }
+
+        return maxLengthOfString
     }
 }
