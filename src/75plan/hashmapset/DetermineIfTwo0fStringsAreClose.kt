02d0a3e0e15ec3fb8cdcb6bf1c64f58aa9bfa42f
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-06-22 21:05:05 +0100

    Determine If Two 0f Strings Are Close

--- src/75plan/hashmapset/DetermineIfTwo0fStringsAreClose.kt
+++ src/75plan/hashmapset/DetermineIfTwo0fStringsAreClose.kt
@@ -11,7 +11,26 @@ package `75plan`.hashmapset
  */
 
 class DetermineIfTwo0fStringsAreClose {
+    // space O(1) - because max of chars is 26 possibilities / time O(n)
     fun closeStrings(word1: String, word2: String): Boolean {
+        if (word1.length != word2.length) return false
 
+        val map1 = HashMap<Char, Int>()
+        val map2 = HashMap<Char, Int>()
+
+        word1.forEach {
+            map1[it] = map1.getOrDefault(it, 0) + 1
+        }
+
+        word2.forEach {
+            map2[it] = map2.getOrDefault(it, 0) + 1
+        }
+
+        if (map1.keys != map2.keys) return false
+
+        val set1 = map1.values.sorted()
+        val set2 = map2.values.sorted()
+
+        return set1 == set2
     }
 }
