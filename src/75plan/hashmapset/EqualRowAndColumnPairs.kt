f8f1992e40aa33a131d409ed0671e5585c7d6f7a
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-06-23 23:47:43 +0100

    Equal Row And Column Pairs

---
+++ src/75plan/hashmapset/EqualRowAndColumnPairs.kt
@@ -0,0 +1,12 @@
+package `75plan`.hashmapset
+
+/**
+ * Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
+ * A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
+ */
+class EqualRowAndColumnPairs {
+    fun equalPairs(grid: Array<IntArray>): Int {
+        var result = 0
+        return result
+    }
+}
