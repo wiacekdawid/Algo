f675118caf134c3e7f6f391030d6e1007910eea4
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-06-25 21:08:56 +0100

    Equal Row And Column Pairs

--- src/75plan/hashmapset/EqualRowAndColumnPairs.kt
+++ src/75plan/hashmapset/EqualRowAndColumnPairs.kt
@@ -4,12 +4,26 @@ package `75plan`.hashmapset
  * Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
  * A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
  */
+fun main() {
+    val test = EqualRowAndColumnPairs().equalPairs(arrayOf(intArrayOf(3,2,1),intArrayOf(1,7,6),intArrayOf(2,7,7)))
+}
 class EqualRowAndColumnPairs {
     fun equalPairs(grid: Array<IntArray>): Int {
-        var result = 0
-        grid.forEach {
-
+        val result = ArrayList<IntArray>()
+        val mapOfCol = HashMap<IntArray, Int>()
+        grid.forEachIndexed { index, ints ->
+            mapOfCol[ints] = index
         }
-        return result
+
+        for (i in grid.indices) {
+            val row = IntArray(grid.size)
+            for (j in grid.first().indices) {
+                row[j] = grid[j][i]
+            }
+            if (mapOfCol.containsKey(row)) {
+                result.add(row)
+            }
+        }
+        return result.size
     }
 }
