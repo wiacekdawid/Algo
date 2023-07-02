508f1d263ebe313f6603a77aea75b5b59b4217e5
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-07-02 21:46:33 +0100

    Equal Row And Column Pairs - Trie

--- src/75plan/hashmapset/EqualRowAndColumnPairs.kt
+++ src/75plan/hashmapset/EqualRowAndColumnPairs.kt
@@ -30,14 +30,54 @@ class EqualRowAndColumnPairs {
         return result
     }
 
-    internal class TrieNode {
+    // solutios using Trie
+    fun equalPairs2(grid: Array<IntArray>): Int {
+        val myTrie = Trie()
         var count = 0
-        var children: Map<Int, TrieNode> = HashMap()
+
+        grid.forEach {
+            myTrie.insert(it)
+        }
+
+        for (i in grid.indices) {
+            val row = IntArray(grid.size)
+            for (j in grid.first().indices) {
+                row[j] = grid[j][i]
+            }
+            count += myTrie.search(row);
+        }
+
+        return count
     }
 
-    internal class Trie(trie: TrieNode = TrieNode()) {
-        fun insert(array: IntArray) {
+    internal class TrieNode {
+        var count = 0
+        var children: HashMap<Int, TrieNode> = HashMap()
+    }
 
+    internal class Trie(private val trie: TrieNode = TrieNode()) {
+
+        fun insert(array: IntArray) {
+//            var myTrie = this.trie
+            array.forEach {
+                if (!trie.children.containsKey(it)) {
+                    trie.children[it] = TrieNode()
+                }
+//                myTrie = myTrie.children[it] ?: TrieNode()
+            }
+            trie.count += 1
+        }
+
+        fun search(array: IntArray): Int {
+            var myTrie = trie
+            array.forEach {
+                if (myTrie.children.containsKey(it)) {
+                    myTrie = myTrie.children[it] ?: TrieNode()
+                } else {
+                    return 0
+                }
+            }
+            return myTrie.count
         }
     }
 }
