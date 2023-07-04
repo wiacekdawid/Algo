7043e0a86612d4cfd2f9847f21540294107a642c
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-07-04 20:54:58 +0100

    Removing Stars From a String

--- src/75plan/stack/RemovingStarsFromAString.kt
+++ src/75plan/stack/RemovingStarsFromAString.kt
@@ -1,5 +1,7 @@
 package `75plan`.stack
 
+import java.util.*
+
 /**
  * You are given a string s, which contains stars *.
  * In one operation, you can:
@@ -11,7 +13,23 @@ package `75plan`.stack
  * It can be shown that the resulting string will always be unique.
  */
 class RemovingStarsFromAString {
+    // time / space O(n)
     fun removeStars(s: String): String {
+        val stack = Stack<Char>()
+        s.forEach {
+            if (it == '*') {
+                if (stack.isNotEmpty()) {
+                    stack.pop()
+                }
+            } else {
+                stack.add(it)
+            }
+        }
 
+        val sb = StringBuilder()
+        while(stack.isNotEmpty()) {
+            sb.append(stack.pop())
+        }
+        return sb.reverse().toString()
     }
 }
