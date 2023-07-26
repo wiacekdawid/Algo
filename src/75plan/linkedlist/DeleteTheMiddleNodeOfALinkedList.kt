6c369a84a764f3cc30b029900feda5d44f23b654
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-07-26 23:19:08 +0100

    Delete the Middle Node of a Linked List

--- src/75plan/linkedlist/DeleteTheMiddleNodeOfALinkedList.kt
+++ src/75plan/linkedlist/DeleteTheMiddleNodeOfALinkedList.kt
@@ -1,7 +1,17 @@
 package `75plan`.linkedlist
 
+/**
+ * You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+ * The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+ * For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+ */
+
 class DeleteTheMiddleNodeOfALinkedList {
     fun deleteMiddle(head: ListNode?): ListNode? {
 
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
     }
 }
