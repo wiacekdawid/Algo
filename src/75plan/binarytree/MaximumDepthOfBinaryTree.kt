9c39269f5341ed5565eac04d5ae787acdc7486e5
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-08-06 20:49:21 +0100

    Maximum Depth Of Binary Tree

---
+++ src/75plan/binarytree/MaximumDepthOfBinaryTree.kt
@@ -0,0 +1,17 @@
+package `75plan`.binarytree
+
+/**
+ * Given the root of a binary tree, return its maximum depth.
+ * A binary tree's maximum depth is the number of nodes along the longest
+ * path from the root node down to the farthest leaf node.
+ */
+class MaximumDepthOfBinaryTree {
+    fun maxDepth(root: TreeNode?): Int {
+        return 0
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+}
