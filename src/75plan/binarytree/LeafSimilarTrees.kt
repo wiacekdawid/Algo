5b4770a09b1384cc12a1ecc043b0cd5be94532a6
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-08-11 21:48:57 +0100

    Leaf-Similar Trees

--- src/75plan/binarytree/LeafSimilarTrees.kt
+++ src/75plan/binarytree/LeafSimilarTrees.kt
@@ -1,4 +1,16 @@
 package `75plan`.binarytree
 
+/**
+ * Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+ */
 class LeafSimilarTrees {
+
+    fun leafSimilar(root1: TreeNode?, root2: TreeNode?): Boolean {
+        return true
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
 }
