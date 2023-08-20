2c7ac8257c9873be3478371fca11372069324f1e
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-08-20 10:30:12 +0100

    Path Sum III

---
+++ src/75plan/binarytree/PathSumIII.kt
@@ -0,0 +1,16 @@
+package `75plan`.binarytree
+
+/**
+ * Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+ * The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+ */
+class PathSumIII {
+    fun pathSum(root: TreeNode?, targetSum: Int): Int {
+
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+}
