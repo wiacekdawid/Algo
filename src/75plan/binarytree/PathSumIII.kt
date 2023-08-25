d7ad078e26cc6efae5f5f43be3afd03f43b13105
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-08-25 04:47:27 +0100

    Path Sum III

--- src/75plan/binarytree/PathSumIII.kt
+++ src/75plan/binarytree/PathSumIII.kt
@@ -27,7 +27,7 @@ fun main() {
 }
 
 class PathSumIII {
-    // prefix sum solution
+    // prefix sum solution time/space O(n)
     fun pathSum(root: TreeNode?, targetSum: Int): Int {
         return checkSum(root, mutableListOf(), targetSum)
     }
