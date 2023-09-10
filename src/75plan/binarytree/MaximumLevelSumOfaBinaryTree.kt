d612a815c4199d92923c5927dc4194c9d53e9480
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-09-10 20:26:57 +0100

    Maximum Level Sum of a Binary Tree

---
+++ src/75plan/binarytree/MaximumLevelSumOfaBinaryTree.kt
@@ -0,0 +1,17 @@
+package `75plan`.binarytree
+
+/**
+ * Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+ * Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+ */
+
+class MaximumLevelSumOfaBinaryTree {
+    fun maxLevelSum(root: TreeNode?): Int {
+
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+}
