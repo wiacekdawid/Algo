dbe46b968b4debe7a5c8c56b1cf305ca2475efdb
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-09-11 23:23:54 +0100

    Maximum Level Sum of a Binary Tree

--- src/75plan/binarytree/MaximumLevelSumOfaBinaryTree.kt
+++ src/75plan/binarytree/MaximumLevelSumOfaBinaryTree.kt
@@ -1,13 +1,39 @@
 package `75plan`.binarytree
 
+import java.util.*
+
 /**
  * Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
  * Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
  */
 
 class MaximumLevelSumOfaBinaryTree {
+    // time/space O(n)
     fun maxLevelSum(root: TreeNode?): Int {
+        val queue = LinkedList<TreeNode>()
+        root?.let { queue.add(root) }
 
+        var currentLevel = 1
+        var maxLevel = currentLevel
+        var maxSumInLevel = Integer.MIN_VALUE
+        while (queue.isNotEmpty()) {
+            val currentSize = queue.size
+            var currentSum = 0
+            for (i in currentSize downTo 1) {
+                val currentNode = queue.poll()
+                currentSum += currentNode.`val`
+                currentNode.left?.let { queue.add(it) }
+                currentNode.right?.let { queue.add(it) }
+            }
+
+            if (currentSum > maxSumInLevel) {
+                maxSumInLevel = currentSum
+                maxLevel = currentLevel
+            }
+            currentLevel++
+        }
+
+        return maxLevel
     }
 
     class TreeNode(var `val`: Int) {
