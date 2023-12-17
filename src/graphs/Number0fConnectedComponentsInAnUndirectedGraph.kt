5a0355d82a9089e65760c826352c7237ed0203ec
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2023-12-17 23:54:00 +0000

    Number0fConnectedComponentsInAnUndirectedGraph - disjoint union set solution

--- src/graphs/Number0fConnectedComponentsInAnUndirectedGraph.kt
+++ src/graphs/Number0fConnectedComponentsInAnUndirectedGraph.kt
@@ -36,7 +36,9 @@ class Number0fConnectedComponentsInAnUndirectedGraph {
         val _vertex1 = find(representative, vertex1)
         val _vertex2 = find(representative, vertex2)
 
-        if (_vertex1 == _vertex2)
+        if (_vertex1 == _vertex2) {
+
+        }
         return 0
     }
 
