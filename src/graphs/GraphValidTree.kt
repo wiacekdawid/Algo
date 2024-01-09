11047ae5aec5a399d1d1148e9e957dc2564e1e02
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-01-09 21:29:15 +0000

    Graph Valid Tree

---
+++ src/graphs/GraphValidTree.kt
@@ -0,0 +1,12 @@
+package graphs
+
+/**
+ * You have a graph of n nodes labeled from 0 to n - 1. You are given an integer n and a list of edges where edges[i] = [ai, bi]
+ * indicates that there is an undirected edge between nodes ai and bi in the graph.
+ * Return true if the edges of the given graph make up a valid tree, and false otherwise.
+ */
+class GraphValidTree {
+    fun validTree(n: Int, edges: Array<IntArray>): Boolean {
+
+    }
+}
