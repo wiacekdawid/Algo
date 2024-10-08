468d6ce0b9949fd45d2011e13cf27f0bee16f25f
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-10-08 22:47:20 +0100

    Jump Game - solution

--- src/2024_restart/noplan/JumpGame.kt
+++ src/2024_restart/noplan/JumpGame.kt
@@ -5,8 +5,26 @@ package `2024_restart`.noplan
  * and each element in the array represents your maximum jump length at that position.
  * Return true if you can reach the last index, or false otherwise.
  */
+
+// time O(n) / space O(1)
 class JumpGame {
     fun canJump(nums: IntArray): Boolean {
+        var farthest = 0
 
+        for (i in nums.indices) {
+            // If we can't reach this position, return false
+            if (i > farthest) {
+                return false
+            }
+            // Update the farthest reachable index
+            farthest = maxOf(farthest, i + nums[i])
+
+            // Early exit if we can already reach the last index
+            if (farthest >= nums.lastIndex) {
+                return true
+            }
+        }
+
+        return true
     }
 }
