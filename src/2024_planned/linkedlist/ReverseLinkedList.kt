8d21f01cafc7283a190ca3d7d800bd629f730d10
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-11-04 10:40:58 +0000

    Reverse Linked List

---
+++ src/2024_planned/linkedlist/ReverseLinkedList.kt
@@ -0,0 +1,26 @@
+package `2024_planned`.linkedlist
+
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ */
+
+// time O(n) / space O(1)
+class ReverseLinkedList {
+    fun reverseList(head: ListNode?): ListNode? {
+        var currentNode = head
+        var currentNext = head?.next
+        var previous: ListNode? = null
+        while (currentNode != null) {
+            currentNext = currentNode.next
+            currentNode.next = previous
+            previous = currentNode
+            currentNode = currentNext
+
+        }
+        return currentNode
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+}
