c2a4696907c7334c60b3bbdb7b72b9222cc54157
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-11-05 13:35:57 +0000

    Remove Nth Node From End Of List

---
+++ src/2024_planned/linkedlist/RemoveNthNodeFromEndOfList.kt
@@ -0,0 +1,35 @@
+package `2024_planned`.linkedlist
+
+/**
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ */
+
+// time O(n) / space O(1)
+class RemoveNthNodeFromEndOfList {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        val dummy = ListNode(0) // Dummy node to handle edge cases
+        dummy.next = head
+        var first: ListNode? = dummy
+        var second: ListNode? = dummy
+
+        // Move the first pointer n+1 steps ahead
+        for (i in 0..n) {
+            first = first?.next
+        }
+
+        // Move both pointers until the first pointer reaches the end
+        while (first != null) {
+            first = first.next
+            second = second?.next
+        }
+
+        // Now, second pointer is just before the node to be deleted
+        second?.next = second?.next?.next
+
+        return dummy.next // Return the modified list, skipping dummy
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+}
