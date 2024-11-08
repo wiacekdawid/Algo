6cfd8a2593a143072f14e4536cd4012d59d975f9
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-11-08 22:22:56 +0000

    Reverse Nodes In k-Group

--- src/2024_planned/linkedlist/ReverseNodesInk-Group.kt
+++ src/2024_planned/linkedlist/ReverseNodesInk-Group.kt
@@ -1,7 +1,5 @@
 package `2024_planned`.linkedlist
 
-import java.util.*
-
 /**
  * Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
  * k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
