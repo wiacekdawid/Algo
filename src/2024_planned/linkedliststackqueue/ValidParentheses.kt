3aa72286aadedf36f7a670bd86682c48775097a8
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-11-09 15:23:14 +0000

    Valid Parentheses

--- src/2024_planned/linkedliststackqueue/ValidParentheses.kt
+++ src/2024_planned/linkedliststackqueue/ValidParentheses.kt
@@ -1,4 +1,27 @@
 package `2024_planned`.linkedliststackqueue
 
+import java.util.*
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * An input string is valid if:
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ */
+
+// time O(n) / space O(n)
 class ValidParentheses {
+    fun isValid(s: String): Boolean {
+        val stack = ArrayDeque<Char>()
+        s.forEach {
+            when (it) {
+                '(', '{', '[' -> stack.addLast(it)
+                ')' -> if (stack.isEmpty() || stack.removeLast() != '(') return false
+                '}' -> if (stack.isEmpty() || stack.removeLast() != '{') return false
+                ']' -> if (stack.isEmpty() || stack.removeLast() != '[') return false
+            }
+        }
+        return stack.isEmpty()
+    }
 }
