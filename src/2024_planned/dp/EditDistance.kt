cc8aa16ad2fc3aa09662d7a71456050dccc3675b
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-11-19 23:16:17 +0000

    Edit Distance

---
+++ src/2024_planned/dp/EditDistance.kt
@@ -0,0 +1,12 @@
+package `2024_planned`.dp
+
+/**
+ * Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+ * You have the following three operations permitted on a word:
+ * Insert a character / Delete a character / Replace a character
+ */
+class EditDistance {
+    fun minDistance(word1: String, word2: String): Int {
+
+    }
+}
