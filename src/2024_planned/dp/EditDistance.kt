1971a8ae9c2ff42d2696e47bad9d7ab6287fb177
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-11-20 20:42:58 +0000

    Edit Distance

--- src/2024_planned/dp/EditDistance.kt
+++ src/2024_planned/dp/EditDistance.kt
@@ -5,8 +5,29 @@ package `2024_planned`.dp
  * You have the following three operations permitted on a word:
  * Insert a character / Delete a character / Replace a character
  */
+
+// time / space O(m x n)
 class EditDistance {
     fun minDistance(word1: String, word2: String): Int {
+        val m = word1.length
+        val n = word2.length
+        val dp = Array(m + 1) { IntArray(n + 1) }
 
+        // Base cases
+        for (i in 0..m) dp[i][0] = i
+        for (j in 0..n) dp[0][j] = j
+
+        // Fill DP table
+        for (i in 1..m) {
+            for (j in 1..n) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1]
+                } else {
+                    dp[i][j] = 1 + minOf(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+                }
+            }
+        }
+
+        return dp[m][n]
     }
 }
