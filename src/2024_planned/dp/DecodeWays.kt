ada265ad6fe7c16dea20c1074b9f4758010e4bfa
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-11-21 12:25:55 +0000

    Decode Ways

--- src/2024_planned/dp/DecodeWays.kt
+++ src/2024_planned/dp/DecodeWays.kt
@@ -12,8 +12,29 @@ package `2024_planned`.dp
  * The test cases are generated so that the answer fits in a 32-bit integer.
  */
 
+// time / space O(n)
 class DecodeWays {
     fun numDecodings(s: String): Int {
+        if (s.isEmpty() || s[0] == '0') return 0
 
+        val n = s.length
+        val dp = IntArray(n + 1) { 0 }
+        dp[0] = 1 // Base case: empty string
+        dp[1] = 1 // Base case: first character is valid
+
+        for (i in 2..n) {
+            val oneDigit = s.substring(i - 1, i).toInt()
+            val twoDigits = s.substring(i - 2, i).toInt()
+
+            if (oneDigit in 1..9) {
+                dp[i] += dp[i - 1] // Add ways from previous single digit
+            }
+
+            if (twoDigits in 10..26) {
+                dp[i] += dp[i - 2] // Add ways from previous two digits
+            }
+        }
+
+        return dp[n]
     }
 }
