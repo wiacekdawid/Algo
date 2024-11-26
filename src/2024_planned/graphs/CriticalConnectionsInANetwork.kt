6a8b65b4466d56babe277cbebf2ba4068c3b0e33
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-11-26 22:34:44 +0000

    Critical Connections In A Network

---
+++ src/2024_planned/graphs/CriticalConnectionsInANetwork.kt
@@ -0,0 +1,13 @@
+package `2024_planned`.graphs
+
+/**
+ * There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections forming a network where connections[i] = [ai, bi]
+ * represents a connection between servers ai and bi. Any server can reach other servers directly or indirectly through the network.
+ * A critical connection is a connection that, if removed, will make some servers unable to reach some other server.
+ * Return all critical connections in the network in any order.
+ */
+class CriticalConnectionsInANetwork {
+    fun criticalConnections(n: Int, connections: List<List<Int>>): List<List<Int>> {
+
+    }
+}
