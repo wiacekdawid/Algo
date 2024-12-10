a8947b3208c8731acbfe41b8dddafadd3cf58f5a
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-10 20:04:14 +0000

    House Robber

--- src/2024_sumUp3Weeks/dp/HouseRobber.kt
+++ src/2024_sumUp3Weeks/dp/HouseRobber.kt
@@ -6,8 +6,26 @@ package `2024_sumUp3Weeks`.dp
  * and it will automatically contact the police if two adjacent houses were broken into on the same night.
  * Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
  */
+
+// time / space O(n)
 class HouseRobber {
     fun rob(nums: IntArray): Int {
+        val memo = IntArray(size = nums.size) { -1 }
 
+        fun helper(currentHouse: Int): Int {
+            if (currentHouse >= nums.size)
+                return 0
+
+            if (memo[currentHouse] != -1)
+                return memo[currentHouse]
+
+            memo[currentHouse] = maxOf(
+                helper(currentHouse + 1), // Skip the current house
+                nums[currentHouse] + helper(currentHouse + 2) // Rob the current house
+            )
+            return memo[currentHouse]
+        }
+
+        return helper(0)
     }
 }
