dc251c4be7a3ad91b2a2d96cc3d83958177b5bf3
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-11 12:35:56 +0000

    House Robber II

--- src/2024_sumUp3Weeks/dp/HouseRobberII.kt
+++ src/2024_sumUp3Weeks/dp/HouseRobberII.kt
@@ -7,8 +7,30 @@ package `2024_sumUp3Weeks`.dp
  * Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
  */
 
+// time / space O(n)
 class HouseRobberII {
     fun rob(nums: IntArray): Int {
+        if (nums.size == 1) return nums[0] // Edge case: only one house
 
+        fun helper(start: Int, end: Int): Int {
+            val memo = IntArray(size = nums.size) { -1 }
+
+            fun robFrom(currentHouse: Int): Int {
+                if (currentHouse > end) return 0
+                if (memo[currentHouse] != -1) return memo[currentHouse]
+
+                memo[currentHouse] = maxOf(
+                    robFrom(currentHouse + 1), // Skip the current house
+                    nums[currentHouse] + robFrom(currentHouse + 2) // Rob the current house
+                )
+                return memo[currentHouse]
+            }
+
+            return robFrom(start)
+        }
+
+        // Scenario 1: Rob houses from 0 to n-2
+        // Scenario 2: Rob houses from 1 to n-1
+        return maxOf(helper(0, nums.size - 2), helper(1, nums.size - 1))
     }
 }
