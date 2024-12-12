b5c30aab9560e671e5c5ea5bb5628fcf736e3de5
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-12 21:44:45 +0000

    Symmetric Tree

---
+++ src/2024_sumUp3Weeks/dp/SymmetricTree.kt
@@ -0,0 +1,25 @@
+package `2024_sumUp3Weeks`.dp
+
+/**
+ * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+ */
+
+// time 0(n) / space O(h)
+class SymmetricTree {
+    fun isSymmetric(root: TreeNode?): Boolean {
+        return root == null || isMirror(root.left, root.right)
+    }
+
+    private fun isMirror(left: TreeNode?, right: TreeNode?): Boolean {
+        if (left == null && right == null) return true
+        if (left == null || right == null) return false
+        return (left.`val` == right.`val`) &&
+                isMirror(left.left, right.right) &&
+                isMirror(left.right, right.left)
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+}
