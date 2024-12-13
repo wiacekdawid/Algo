9b3dd7fb37abd3e600cabc8fa3541c6a7481c64d
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-13 21:32:54 +0000

    Path Sum

---
+++ src/2024_sumUp3Weeks/dp/PathSum.kt
@@ -0,0 +1,17 @@
+package `2024_sumUp3Weeks`.dp
+
+/**
+ * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+ * A leaf is a node with no children.
+ */
+
+class PathSum {
+    fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
+
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+}
