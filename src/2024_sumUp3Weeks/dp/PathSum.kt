e2b65bfa0150b20390202924f1fd7ddcf6d1e315
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-14 15:43:57 +0000

    Path Sum

--- src/2024_sumUp3Weeks/dp/PathSum.kt
+++ src/2024_sumUp3Weeks/dp/PathSum.kt
@@ -5,9 +5,26 @@ package `2024_sumUp3Weeks`.dp
  * A leaf is a node with no children.
  */
 
+// time O(n) / space O(h)
 class PathSum {
     fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
+        fun helper(currentSum: Int, node: TreeNode?): Boolean {
+            if (node == null) return false
+            val newSum = currentSum + node.`val`
+            if (newSum == targetSum && node.left == null && node.right == null)
+                return true
+            return helper(newSum, node.left) || helper(newSum, node.right)
+        }
+        return helper(0, root)
+    }
 
+    fun hasPathSum2(root: TreeNode?, targetSum: Int): Boolean {
+        if (root == null) return false
+        if (root.left == null && root.right == null) {
+            return targetSum == root.`val`
+        }
+        val newTarget = targetSum - root.`val`
+        return hasPathSum(root.left, newTarget) || hasPathSum(root.right, newTarget)
     }
 
     class TreeNode(var `val`: Int) {
