983cffa2f3995c66cad62580abcfae376a114db0
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-26 23:17:23 +0000

    Intersection Of Two Arrays

---
+++ src/2024_summary/IntersectionOfTwoArrays.kt
@@ -0,0 +1,11 @@
+package `2024_summary`
+
+/**
+ * Given two integer arrays nums1 and nums2, return an array of their intersection.
+ * Each element in the result must be unique and you may return the result in any order.
+ */
+class IntersectionOfTwoArrays {
+    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+
+    }
+}
