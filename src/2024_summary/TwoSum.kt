b4ce20da2ebb84dc2ddf47c4bc3e4b0fddf410e3
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-26 17:25:47 +0000

    Two Sum

---
+++ src/2024_summary/TwoSum.kt
@@ -0,0 +1,24 @@
+package `2024_summary`
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.\
+ * You can return the answer in any order.
+ */
+
+class TwoSum {
+    // time / space O(n)
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val complementMap = HashMap<Int, Int>()
+
+        nums.forEachIndexed { index, i ->
+            val current = target - i
+            if (complementMap.containsKey(current)) {
+                return intArrayOf(complementMap.getValue(current), index)
+            } else {
+                complementMap[i] = index
+            }
+        }
+        throw IllegalStateException("No 2 sum found in given array")
+    }
+}
