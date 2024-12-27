acd080fb8c024a8250746e9d88a8b2ff4b683159
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-27 20:10:51 +0000

    Intersection Of Two Arrays

--- src/2024_summary/IntersectionOfTwoArrays.kt
+++ src/2024_summary/IntersectionOfTwoArrays.kt
@@ -5,7 +5,10 @@ package `2024_summary`
  * Each element in the result must be unique and you may return the result in any order.
  */
 class IntersectionOfTwoArrays {
+    // time O(n+m) / space O(n+k) where n/m are lengts of num1/num2 and k is length of intersection
     fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
-
+        val numSet1 = nums1.toSet()
+        val outputSet = nums2.filter { it in numSet1 }.toSet()
+        return outputSet.toIntArray()
     }
 }
