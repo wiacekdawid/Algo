b30cd9c1db2b6ec49dd1385174e062d4b7d1b3c1
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-29 23:48:41 +0000

    Permutations

---
+++ src/2024_summary/Permutations.kt
@@ -0,0 +1,11 @@
+package `2024_summary`
+
+/**
+ * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+ */
+
+class Permutations {
+    fun permute(nums: IntArray): List<List<Int>> {
+
+    }
+}
