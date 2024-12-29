ac52ba4c4624c419ab22cb85c6b5ef901204abdd
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-29 23:57:08 +0000

    Permutations

--- src/2024_summary/Permutations.kt
+++ src/2024_summary/Permutations.kt
@@ -6,6 +6,21 @@ package `2024_summary`
 
 class Permutations {
     fun permute(nums: IntArray): List<List<Int>> {
+        val output = mutableListOf<List<Int>>()
 
+        fun backtrack(currentIndex, currentList: MutableList) {
+            if (currentIndex == nums.size) {
+                output.add(currentList.toList())
+            }
+
+            for (i in currentIndex until nums.size) {
+                currentList.add(nums[1])
+                backtrack(i, currentList)
+                currentList.removeAt(currentList.lastIndex)
+            }
+        }
+
+        backtrack(0, mutableListOf<Int>())
+        return output.toList()
     }
 }
