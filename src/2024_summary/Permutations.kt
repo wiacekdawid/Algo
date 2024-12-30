2eeedb9453e80efa060196fb40fcc717a03a159b
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2024-12-30 18:52:20 +0000

    Permutations

--- src/2024_summary/Permutations.kt
+++ src/2024_summary/Permutations.kt
@@ -4,23 +4,28 @@ package `2024_summary`
  * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
  */
 
+// time O(n x n!) / space O(n) for recursion stack
 class Permutations {
     fun permute(nums: IntArray): List<List<Int>> {
         val output = mutableListOf<List<Int>>()
 
-        fun backtrack(currentIndex, currentList: MutableList) {
-            if (currentIndex == nums.size) {
-                output.add(currentList.toList())
+        fun backtrack(start: Int) {
+            if (start == nums.size) {
+                output.add(nums.toList())
+                return
             }
 
-            for (i in currentIndex until nums.size) {
-                currentList.add(nums[1])
-                backtrack(i, currentList)
-                currentList.removeAt(currentList.lastIndex)
+            for (i in start until nums.size) {
+                // Swap elements at `start` and `i`
+                nums[start] = nums[i].also { nums[i] = nums[start] }
+                // Recur with the next position
+                backtrack(start + 1)
+                // Backtrack: undo the swap
+                nums[start] = nums[i].also { nums[i] = nums[start] }
             }
         }
 
-        backtrack(0, mutableListOf<Int>())
-        return output.toList()
+        backtrack(0)
+        return output
     }
 }
