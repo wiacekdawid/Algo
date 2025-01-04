7ff8a7f79c55344e0719c99d37546dfd51b85b95
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-01-04 21:01:53 +0000

    Container With Most Water

---
+++ src/2025/g/array_strings/ContainerWithMostWater.kt
@@ -0,0 +1,14 @@
+package `2025`.g.array_strings
+
+/**
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ * Return the maximum amount of water a container can store.
+ * Notice that you may not slant the container.
+ */
+
+class ContainerWithMostWater {
+    fun maxArea(height: IntArray): Int {
+
+    }
+}
