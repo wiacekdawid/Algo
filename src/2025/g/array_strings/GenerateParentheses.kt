d2c548bc776d708723a3912df9b3c4b812ec589c
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-01-05 21:20:14 +0000

    Generate Parentheses

---
+++ src/2025/g/array_strings/GenerateParentheses.kt
@@ -0,0 +1,11 @@
+package `2025`.g.array_strings
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ */
+
+class GenerateParentheses {
+    fun generateParenthesis(n: Int): List<String> {
+
+    }
+}
