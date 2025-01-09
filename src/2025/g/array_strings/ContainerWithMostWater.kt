e9ddfc59f436bc9c8218730efad6345da9c832f6
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-01-09 21:01:37 +0000

    Container With Most Water

--- src/2025/g/array_strings/ContainerWithMostWater.kt
+++ src/2025/g/array_strings/ContainerWithMostWater.kt
@@ -8,7 +8,27 @@ package `2025`.g.array_strings
  */
 
 class ContainerWithMostWater {
+    // time O(n) / space O(1)
     fun maxArea(height: IntArray): Int {
+        if (height.size < 2) return 0
 
+        var currentMax = 0
+        var leftIndex = 0
+        var rightIndex = height.size - 1
+
+        while (leftIndex < rightIndex) {
+            // Calculate the current area
+            val currentLevel = height[leftIndex].coerceAtMost(height[rightIndex])
+            currentMax = currentMax.coerceAtLeast((rightIndex - leftIndex) * currentLevel)
+
+            // Move the pointer corresponding to the smaller height
+            if (height[leftIndex] < height[rightIndex]) {
+                leftIndex++
+            } else {
+                rightIndex--
+            }
+        }
+
+        return currentMax
     }
 }
