6bc78b002f1b732a6714cf1291d4a46c04551b84
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-01-10 20:12:31 +0000

    Number Of Islands

--- src/2025/g/treesgraphs/NumberOfIslands.kt
+++ src/2025/g/treesgraphs/NumberOfIslands.kt
@@ -6,7 +6,31 @@ package `2025`.g.treesgraphs
  * You may assume all four edges of the grid are all surrounded by water.
  */
 class NumberOfIslands {
+    // time / space O(m x n)
     fun numIslands(grid: Array<CharArray>): Int {
+        if (grid.isEmpty()) return 0
+        var numOfIslands = 0
+        val rows = grid.size
+        val cols = grid[0].size
 
+        fun exploreCurrentIsland(x: Int, y: Int) {
+            if (x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] != '1') return
+            grid[x][y] = '.'
+            exploreCurrentIsland(x - 1, y)
+            exploreCurrentIsland(x + 1, y)
+            exploreCurrentIsland(x, y - 1)
+            exploreCurrentIsland(x, y + 1)
+        }
+
+        for (x in grid.indices) {
+            for (y in grid[x].indices) {
+                if (grid[x][y] == '1') {
+                    numOfIslands++
+                    exploreCurrentIsland(x, y)
+                }
+            }
+        }
+
+        return numOfIslands
     }
 }
