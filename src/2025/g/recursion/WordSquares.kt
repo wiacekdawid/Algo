bac75432b6ea08972020e9daee517f4162f7c546
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-01-11 23:22:24 +0000

    Word Squares

---
+++ src/2025/g/recursion/WordSquares.kt
@@ -0,0 +1,13 @@
+package `2025`.g.recursion
+
+/**
+ * Given an array of unique strings words, return all the word squares you can build from words. The same word from words can be used multiple times. You can return the answer in any order.
+ * A sequence of strings forms a valid word square if the kth row and column read the same string, where 0 <= k < max(numRows, numColumns).
+ * For example, the word sequence ["ball","area","lead","lady"] forms a word square because each word reads the same both horizontally and vertically.
+ */
+
+class WordSquares {
+    fun wordSquares(words: Array<String>): List<List<String>> {
+
+    }
+}
