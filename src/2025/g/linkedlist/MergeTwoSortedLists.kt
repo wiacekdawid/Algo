4323657bf3c696a9a270436cc3ca915ee9804f9f
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-01-13 23:39:49 +0000

    Merge Two Sorted Lists

---
+++ src/2025/g/linkedlist/MergeTwoSortedLists.kt
@@ -0,0 +1,17 @@
+package `2025`.g.linkedlist
+
+/**
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ * Return the head of the merged linked list.
+ */
+
+class MergeTwoSortedLists {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+}
