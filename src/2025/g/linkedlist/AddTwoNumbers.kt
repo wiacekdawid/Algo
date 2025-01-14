4dc8fc245e34b4df7570fa279f7ecd9cba9a5ca9
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-01-14 20:41:39 +0000

    Add Two Numbers

---
+++ src/2025/g/linkedlist/AddTwoNumbers.kt
@@ -0,0 +1,16 @@
+package `2025`.g.linkedlist
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+ * and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
+class AddTwoNumbers {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+}
