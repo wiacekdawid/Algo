39e69227872d2ba8aac0a7d261bdf2bdef5bf8c0
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-01-17 19:11:19 +0000

    Remove Nth Node From End Of List

---
+++ src/2025/g/linkedlist/RemoveNthNodeFromEndOfList.kt
@@ -0,0 +1,15 @@
+package `2025`.g.linkedlist
+
+/**
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ */
+
+class RemoveNthNodeFromEndOfList {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+}
