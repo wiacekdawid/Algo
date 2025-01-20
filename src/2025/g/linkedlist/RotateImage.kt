11c2eb8f8a6ef92da3e8479d80822d3f5442dcf2
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-01-20 23:56:33 +0000

    Rotate Image

---
+++ src/2025/g/linkedlist/RotateImage.kt
@@ -0,0 +1,12 @@
+package `2025`.g.linkedlist
+
+/**
+ * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+ * DO NOT allocate another 2D matrix and do the rotation.
+ */
+class RotateImage {
+    fun rotate(matrix: Array<IntArray>): Unit {
+
+    }
+}
