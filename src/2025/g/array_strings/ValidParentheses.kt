8d5ef01a23d1063610b64168d60d564151aa5b0e
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-01-23 23:56:47 +0000

    Valid Parentheses

---
+++ src/2025/g/array_strings/ValidParentheses.kt
@@ -0,0 +1,14 @@
+package `2025`.g.array_strings
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * An input string is valid if:
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ */
+class ValidParentheses {
+    fun isValid(s: String): Boolean {
+
+    }
+}
