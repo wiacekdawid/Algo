8e9f8a231c624fe2c7477a5c91eb1dd177cee5d7
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-02-01 22:03:13 +0000

    Rotate Image

--- src/2025/g/array_strings/RotateImage.kt
+++ src/2025/g/array_strings/RotateImage.kt
@@ -5,7 +5,22 @@ package `2025`.g.array_strings
  * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
  */
 class RotateImage {
+    // time O(pow n) / space O(1)
     fun rotate(matrix: Array<IntArray>): Unit {
+        val n = matrix.size
 
+        // Step 1: Transpose the matrix
+        for (i in 0 until n) {
+            for (j in i + 1 until n) {
+                val temp = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = temp
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (row in matrix) {
+            row.reverse()
+        }
     }
 }
