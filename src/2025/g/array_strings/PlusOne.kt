2a10840374fd713c2369a17341d2e5d051ab3e93
Dawid Wiacek <dawid.wiacek@tui.co.uk>
2025-02-10 23:55:35 +0000

    Plus One

---
+++ src/2025/g/array_strings/PlusOne.kt
@@ -0,0 +1,14 @@
+package `2025`.g.array_strings
+
+/**
+ * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+ * The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+ * Increment the large integer by one and return the resulting array of digits.
+
+ */
+class PlusOne {
+    fun plusOne(digits: IntArray): IntArray {
+        val test  = 1
+        return intarrayOf()
+    }
+}
